056292bda4c146d7ef80d639ba491b52b2ee97e0
Jasmine Carter <jasmine.carter87@gmail.com>
2019-05-28 23:32:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(customers)
     puts "The line is currently empty."
   else
     customers.each do |name|
-      current_line.push(name).split
+      current_line.push(name)
     end
     puts "The line is currently: #{current_line}"
 end
