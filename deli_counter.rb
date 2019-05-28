7b39f3b965606ddfeb2fab17ff4a42985d484f63
Jasmine Carter <jasmine.carter87@gmail.com>
2019-05-28 23:35:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,15 @@
 # Write your code here.
 def line(customers)
   current_line = []
+  position = 1
   if customers.count  == 0
     puts "The line is currently empty."
   else
+    current_line.push(position)
     customers.each do |name|
       current_line.push(name)
+      position = position+1
     end
-    current_line = current_line.join(", ")
     puts "The line is currently: #{current_line}"
 end
 end
