d3e04fcbb7ce5ce4789995849b3c56de77b65225
Jasmine Carter <jasmine.carter87@gmail.com>
2019-05-28 23:27:02 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
 # Write your code here.
+def line(customers)
+current_line = []
+if customers.count  == 0
+puts "The line is currently empty."
+else
+customers.each do |name|
+  puts current_line.push(name)
+end
+
+def take_a_number(current_line, next_person)
+end
