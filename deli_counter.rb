f36d8b9d10b269b0a7de611a36d25a90c500e6ff
Jasmine Carter <jasmine.carter87@gmail.com>
2019-05-28 23:50:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,12 @@
 # Write your code here.
+katz_deli = []
+
 def line(customers)
   current_line = []
   position = 1
   if customers.count  == 0
     puts "The line is currently empty."
   else
-    current_line.push(position)
     customers.each do |name|
       current_line.push(name)
       position = position+1
@@ -14,5 +15,8 @@ def line(customers)
 end
 end
 
-def take_a_number(current_line, next_person)
-end
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  position = katz_deli.index(name) + 1
+  puts "Welcome, #{name}. You are number #{position}"
+end  
