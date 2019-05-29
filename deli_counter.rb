dc0795016bb73a7c3549725eb404517bec8c7c53
Jasmine Carter <jasmine.carter87@gmail.com>
2019-05-29 00:32:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,10 +4,10 @@ katz_deli = []
 def line(katz_deli)
   current_line = []
   value = 0
-  if customers.count == 0
+  if katz_deli.count == 0
     puts "The line is currently empty."
   else
-    customers.each do |name|
+    katz_deli.each do |name|
       value = value + 1
       position = "#{value}."
       current_line.push(position)
