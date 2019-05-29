3062f4259084f07d13ff6c07fa2ac0244f37fc2a
Jasmine Carter <jasmine.carter87@gmail.com>
2019-05-29 00:16:14 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,6 +4,7 @@ katz_deli = []
 def line(customers)
   current_line = []
   value = 1
+  position = "#{value}."
   if customers.count  == 0
     puts "The line is currently empty."
   else
@@ -11,7 +12,6 @@ def line(customers)
       current_line.push(position)
       current_line.push(name)
       value = value + 1
-      position = "#{value}."
   end
     current_line = current_line.join(" ")
     puts "The line is currently: #{current_line}"
