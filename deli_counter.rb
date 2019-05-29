ea97a8be66a0e0ee71a0e3ffa5ac893fbbbccda6
Jasmine Carter <jasmine.carter87@gmail.com>
2019-05-29 00:35:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -29,6 +29,7 @@ def now_serving(katz_deli)
   if katz_deli.count == 0
     puts "There is nobody waiting to be served!"
   else
-    puts "Welcome #{name}"
+    puts "Currently serving #{name}."
+    katz_deli.shift
   end
 end
