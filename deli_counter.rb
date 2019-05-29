036f6e9876362e738a48f82cfa85a3b2e9805ca8
Jasmine Carter <jasmine.carter87@gmail.com>
2019-05-29 00:27:18 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,15 +4,14 @@ katz_deli = []
 def line(customers)
   current_line = []
   value = 1
-  #position = "#{value}."
-  if customers.count  == 0
+  if customers.count == 0
     puts "The line is currently empty."
   else
     customers.each do |name|
-      current_line.push(position)
-      current_line.push(name)
       value = value + 1
       position = "#{value}."
+      current_line.push(position)
+      current_line.push(name)
       #position = value.to_s
   end
     current_line = current_line.join(" ")
