364e4c8061fe98ecc8bd048fe57e6b401139694d
Jasmine Carter <jasmine.carter87@gmail.com>
2019-05-29 00:40:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -31,7 +31,7 @@ def now_serving(katz_deli)
     else
         katz_deli.each do |name|
         puts "Currently serving #{name}."
-        katz_deli.shift
+        #katz_deli.shift
       end
     end
 end
